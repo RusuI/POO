aa92f52a0d31b771a615cf7a184915dc6d52f4ea
RusuI <79109790+RusuI@users.noreply.github.com>
2021-03-28 16:16:16 +0300

    Add files via upload

--- Laborator 6/Car.h
+++ Laborator 6/Car.h
@@ -1,21 +1,15 @@
-#pragma once
+#ifndef CARH
+#define CARH
+
 class Car {
 protected:
-	int fuel_consumption;
-	int fuel_capacity;
-	int avg_speed_RAIN;
-	int avg_speed_SUNNY;
-	int avg_speed_SNOW;
-	const char* name;
-	
-
-	Car();
-	~Car();
-
-	virtual int Getfuel_consuption()=0;
-	virtual int Getfuel_capacity()=0;
-	virtual int Getavg_speed_RAIN()=0;
-	virtual int Getavg_speed_SNOW() = 0;
-	virtual int Getavg_speed_SUNNY() = 0;
-	
+	int fuelCapacity;
+	int fuelConsumption;
+	int averageSpeed[3];
+	char* name;
+public:
+	virtual float GoCar(bool&, int, int) = 0;
+	virtual char* getName() = 0;
 };
+
+#endif // !CARH
